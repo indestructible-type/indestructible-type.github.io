8d485db995ebe8adeaba1b475b99ad2f636b23b9
Owen Earl <EwonRael@yahoo.com>
2022-05-20 16:24:01 +0200

    Besley* 3.0

--- Besley/sliders.js
+++ Besley/sliders.js
@@ -1,11 +1,11 @@
 document.getElementById('wght').oninput = function() {
   if (document.getElementById('itlc').value == 1) {
     document.getElementById('itlct').innerHTML = "Italic";
-    document.getElementById('title').style = "font-family: 'Bodonivfitl'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ";";
+    document.getElementById('title').style = "font-family: 'Bodonivfitl'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ", 'wdth' " + document.getElementById('wdth').value + ";";
   }
   else {
     document.getElementById('itlct').innerHTML = "Roman";
-    document.getElementById('title').style = "font-family: 'Bodonivf'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ";";
+    document.getElementById('title').style = "font-family: 'Bodonivf'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ", 'wdth' " + document.getElementById('wdth').value + ";";
   }
   document.getElementById('wghtt').innerHTML = "Weight: " + document.getElementById('wght').value;
 }
@@ -13,11 +13,23 @@ document.getElementById('wght').oninput = function() {
 document.getElementById('itlc').oninput = function() {
   if (document.getElementById('itlc').value == 1) {
     document.getElementById('itlct').innerHTML = "Italic";
-    document.getElementById('title').style = "font-family: 'Bodonivfitl'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ";";
+    document.getElementById('title').style = "font-family: 'Bodonivfitl'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ", 'wdth' " + document.getElementById('wdth').value + ";";
   }
   else {
     document.getElementById('itlct').innerHTML = "Roman";
-    document.getElementById('title').style = "font-family: 'Bodonivf'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ";";
+    document.getElementById('title').style = "font-family: 'Bodonivf'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ", 'wdth' " + document.getElementById('wdth').value + ";";
   }
   document.getElementById('wghtt').innerHTML = "Weight: " + document.getElementById('wght').value;
+}
+
+document.getElementById('wdth').oninput = function() {
+  if (document.getElementById('itlc').value == 1) {
+    document.getElementById('itlct').innerHTML = "Italic";
+    document.getElementById('title').style = "font-family: 'Bodonivfitl'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ", 'wdth' " + document.getElementById('wdth').value + ";";
+  }
+  else {
+    document.getElementById('itlct').innerHTML = "Roman";
+    document.getElementById('title').style = "font-family: 'Bodonivf'; font-variation-settings: 'wght' " + document.getElementById('wght').value + ", 'wdth' " + document.getElementById('wdth').value + ";";
+  }
+  document.getElementById('wdtht').innerHTML = "Width: " + document.getElementById('wdth').value + "%";
 }
