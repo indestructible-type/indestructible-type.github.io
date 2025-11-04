65e7f153e0c6e39cc49f17bc67e87e103c27637f
EwonRael <ewonrael@yahoo.com>
2025-11-04 10:01:44 -0600

    Marauder

--- Marauder/script.js
+++ Marauder/script.js
@@ -1,5 +1,5 @@
 function parallax() {
-	var position = - (window.scrollY/1.75)
+	var position = (window.scrollY/1.75)
 	document.getElementById("sky").style = "top: "+position+"px;" 
 }
 
@@ -7,4 +7,11 @@ window.onload = function() {
 	var h1 = document.querySelector("h1")
 	var size = window.getComputedStyle(h1, null).getPropertyValue('font-size').slice(0, -2)
 	h1.style = "font-variation-settings: 'opsz' "+size/2+";"
+}
+
+function slider (id, value) {
+	document.getElementById(id).style = "font-variation-settings: '"+id+"' "+value+";"
+	if (id == "opsz") {
+		document.getElementById("opszl").style = "font-size: "+2*value+"px;"
+	}
 }
